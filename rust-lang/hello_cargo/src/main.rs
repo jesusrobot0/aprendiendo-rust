db788c266ec7a7a86859eb53ab9d01045229b8cf
jesusrobot0 <jesusrobot9000@icloud.com>
2024-05-20 15:42:29 -0500

    Add the code of the first chapter: First Steps

---
+++ rust-lang/hello_cargo/src/main.rs
@@ -0,0 +1,11 @@
+// Este proyecto fue creado con cargo, es como el npm del mundo rust.
+// Al igual que su simíl del mundo de js además de servir como gestor
+// de paquetes tambien sirve para inicializar proyectos, esta es la
+// forma más profesional de trabajar.
+// El proyecto se compila con cargo build y ejecuta con cargo run,
+// aunque de igual manera si no se a compilado el programa previamente
+// cargo run lo hace, es un comando dos en uno.
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ rust-lang/hello_world/main.rs
@@ -0,0 +1,6 @@
+// Este es un proyecto creado a mano, solo basta con tener un main.rs.
+// Para compilarlo basta con rustc main.js y ejecutarlo (en mac y linux) ./main
+
+fn main() {
+  println!("¡Hola, mundo!");
+}
